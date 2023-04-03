b3daf72a243f09da5cd25154ff7523b6fb3e2b28
wutche <wutche@pop-os.localdomain>
2023-04-03 17:49:45 +0100

    some changes

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,38 @@
+/** @format */
+
+import { useState } from "react";
+import memesdata from "./memesdata";
+
 const Meme = () => {
-  return (
-    <div className="memeinputs"></div>
-  )
-}
-export default Meme
+	// const [memeImage, setMemeImage] = useState("");
+  const [meme, setMeme] = useState({
+		toptext: "",
+		bottomtext: "",
+		randomimage:
+			"https://i.pinimg.com/736x/46/2f/af/462fafbf63e981c2cc4bd699da32bab6.jpg",
+	});
+
+  const [allMemeData, setAllMemeData] = useState(memesdata)
+
+	function generateImage() {
+		const memesArray = allMemeData.data.memes;
+		const randomMeme = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomMeme].url;
+		setMeme((prevMeme) => ({
+			...prevMeme,
+			randomimage: url,
+		}));
+	}
+	return (
+		<main>
+			<div className="form">
+				<input type="text" placeholder="Top Text" />
+				<input type="text" placeholder="Bottom Text" />
+				<button onClick={generateImage}>Get a new meme image 🖼</button>
+			</div>
+
+      <img src={meme.randomimage} className= "meme--image" alt="" />
+		</main>
+	);
+};
+export default Meme;
